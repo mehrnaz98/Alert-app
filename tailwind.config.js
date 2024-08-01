8cef563392e0b4da6715ec6624343c5d7be5004c
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-01 23:01:59 +0330

    Fix Tailwind CSS setup and linter configuration
    - Updated Tailwind CSS configuration file paths
    - Adjusted linter configuration to recognize Tailwind at-rules
    - Verified `tailwind.config.js` and `.stylelintrc` settings
    - Ensured build tools and dependencies are correctly configured

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/app.vue", "./src/components/app.vue"],
+  content: ["./index.html", "./src/app.vue", "./src/components/AppAlert.vue"],
   theme: {
     extend: {},
   },
