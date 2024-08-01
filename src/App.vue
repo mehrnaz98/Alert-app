c4c69d777ae36199c33f9b5feb79423b472f1d91
mehrnaz98 <mehrnaz.eft98@gmail.com>
2024-08-01 18:19:56 +0330

    Update App.vue
    4 types of app alert components added

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,13 @@
 import AppAlert from "./components/AppAlert.vue";
 </script>
 
-<template></template>
+<template>
+  <div class="p-5">
+    <AppAlert type="info"></AppAlert>
+    <AppAlert type="success"></AppAlert>
+    <AppAlert type="warning"></AppAlert>
+    <AppAlert type="error"></AppAlert>
+  </div>
+</template>
 
 <style scoped></style>
